520c0e0fe90b39c5c829dfd38c89e4853b4a0e3e
Rodrigo do Nascimento <rodrigoguigui@gmail.com>
2023-05-23 16:53:20 -0300

    Adicionando funcionalidade mensagem

--- cadstro.js
+++ cadstro.js
@@ -3,3 +3,9 @@ function signUp(email) {
 }
 
 // Pegando um email e cadastrando na plataforma.
+
+function mostraMensagem() {
+    return "Bem-vindo ao sistema";
+}
+
+// Adicionando funcao de mensagem
